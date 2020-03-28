c15118b09f2a08b4c6d018e2f3b9ea8e611dc093
grabania <grabania.work@gmail.com>
2020-03-28 12:21:57 +0000

    Add split method

--- organize_files.py
+++ organize_files.py
@@ -1,8 +1,19 @@
+# def extract_place(filename):
+#     first = filename.find("_")
+#     partial = filename[first+1:]
+#     second = partial.find("_")
+#     return partial[:second]
+
+
+# print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
+# print(extract_place("2018-01-03_Oahu_21/51/57.jpg"))
+# print(extract_place("2018-01_Scottland_11/51/27.jpg"))
+
+import os
+
+
 def extract_place(filename):
-    first = filename.find("_")
-    partial = filename[first+1:]
-    second = partial.find("_")
-    return partial[:second]
+    return filename.split("_")[1]
 
 
 print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
